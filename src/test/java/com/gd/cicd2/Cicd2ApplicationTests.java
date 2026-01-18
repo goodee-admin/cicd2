cd60c2eeda2844864c8d9dd364d7e10c7876e339
goodee <goodee.project@gmail.com>
2026-01-18 16:23:39 +0900

    test: 첫번째 커밋

---
+++ src/test/java/com/gd/cicd2/Cicd2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gd.cicd2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicd2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
